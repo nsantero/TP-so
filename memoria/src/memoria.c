06222e04c93e97cf6d25876d3a0db1160d3bfac7
camisanchez <camsanchez@frba.utn.edu.ar>
2024-06-17 21:37:26 +0000

    memoria configs

--- memoria/src/memoria.c
+++ memoria/src/memoria.c
@@ -1,34 +1,40 @@
 #include <memoria.h>
 
-void cargar_configuracion(char* archivo_configuracion)
+t_config* configMemoria = NULL;
+t_log* loggerMemoria = NULL;
+memoria_config configuracionMemoria;
+
+void cargar_configuracion()
 {
-	t_config* config = config_create(archivo_configuracion); //Leo el archivo de configuracion
-
-	if (config == NULL) {
-		perror("Archivo de configuracion no encontrado");
-		exit(-1);
-	}
-
-	config_valores.puerto_escucha = config_get_string_value(config, "PUERTO_ESCUCHA");
-    config_valores.tam_memoria = config_get_int_value(config,"TAM_MEMORIA");
-    config_valores.tam_pagina = config_get_int_value(config,"TAM_PAGINA");
-    config_valores.path_instrucciones = config_get_string_value(config,"PATH_INSTRUCCIONES");
-    config_valores.retardo_respuesta = config_get_int_value(config,"RETARDO_RESPUESTA");
+	configMemoria = config_create("../memoria/memoria.config");
+	configuracionMemoria.PUERTO_ESCUCHA = config_get_string_value(configMemoria, "PUERTO_ESCUCHA");
+    configuracionMemoria.TAM_MEMORIA = config_get_int_value(configMemoria,"TAM_MEMORIA");
+    configuracionMemoria.TAM_PAGINA = config_get_int_value(configMemoria,"TAM_PAGINA");
+    configuracionMemoria.PATH_INSTRUCCIONES = config_get_string_value(configMemoria,"PATH_INSTRUCCIONES");
+    configuracionMemoria.RETARDO_RESPUESTA = config_get_int_value(configMemoria,"RETARDO_RESPUESTA");
 	
+    log_info(loggerMemoria, "Configuracion guardada");
 }
+
+void iniciarLoggerMemoria(){
+    loggerMemoria = log_create("../memoria/memoria.log","Memoria", 0, LOG_LEVEL_INFO);
+    log_info(loggerMemoria,"Logger Memoria creado");
+}
+
+void crearListas(){
+    lista_ProcesosActivos = list_create(); 
+}
+
 
 int main(int argc, char *argv[])
 {
+    iniciarLoggerMemoria();
+	cargar_configuracion();
 
-    logger = log_create("../memoria/src/memoria.log", "MEMORIA", true, LOG_LEVEL_INFO);
-	log_info(logger, "Se creo el log!");
+    crearListas();
 
-	cargar_configuracion("/home/utnso/tp-2024-1c-File-System-Fanatics/memoria/memoria.config");
-
-    lista_procesos_activos = list_create(); 
-
-	int server_fd = iniciar_servidor(logger, server_name ,IP, config_valores.puerto_escucha);  //cambiar variable global
-	log_info(logger, "Servidor listo para recibir al cliente");
+	int server_fd = iniciar_servidor(loggerMemoria, server_name ,IP, configuracionMemoria.PUERTO_ESCUCHA );  //cambiar variable global
+	log_info(loggerMemoria, "Servidor listo para recibir al cliente");
 
 	// espero a kernel y cpu
 	while(server_escuchar(server_fd));
@@ -47,14 +53,14 @@ static void procesar_conexion(void *void_args) {
     
 	while (cliente_socket != -1) {
 		if (recv(cliente_socket, &cop, sizeof(op_code), 0) != sizeof(op_code)) {
-			log_info(logger, ANSI_COLOR_BLUE"El cliente se desconecto de %s server", server_name);
+			log_info(loggerMemoria, ANSI_COLOR_BLUE"El cliente se desconecto de %s server", server_name);
 			return;
 		}
 		switch (cop) {
 		case MENSAJE:{
 
 			char* mensaje = recibir_mensaje(cliente_socket);
-            log_info(logger, ANSI_COLOR_YELLOW"Me llegó el mensaje: %s", mensaje);
+            log_info(loggerMemoria, ANSI_COLOR_YELLOW"Me llegó el mensaje: %s", mensaje);
                 
             free(mensaje); // Liberar la memoria del mensaje recibido
 
@@ -65,12 +71,12 @@ static void procesar_conexion(void *void_args) {
             int *pid;
             char *nombre_archivo;
             //recv_inicio_proceso(cliente_socket,&pid,&nombre_archivo);
-            log_info(logger,"nombre archivo: %s", nombre_archivo);
+            log_info(loggerMemoria,"nombre archivo: %s", nombre_archivo);
             t_proceso* proceso = malloc(sizeof(t_proceso));
             proceso->pid = *pid;
             proceso->archivos_instrucciones = nombre_archivo;
             
-            list_add(lista_procesos_activos,proceso);
+            list_add(lista_ProcesosActivos,proceso);
             /*
             free(pid);
             free(nombre_archivo);
@@ -81,7 +87,7 @@ static void procesar_conexion(void *void_args) {
             
             int *pid;
             int *pc;
-            usleep(config_valores.retardo_respuesta *1000);
+            usleep(configuracionMemoria.RETARDO_RESPUESTA *1000);
             recv_fetch_instruccion(cliente_socket, &pid,&pc);
             //leer_instruccion_por_pc_y_enviar(*pid,*pc, cliente_socket);
             free(pid); 
@@ -106,7 +112,7 @@ static void procesar_conexion(void *void_args) {
 
 int server_escuchar(int fd_memoria) {
 	server_name = "Memoria";
-	int cliente_socket = esperar_cliente(logger, server_name, fd_memoria);
+	int cliente_socket = esperar_cliente(loggerMemoria, server_name, fd_memoria);
 
 	if (cliente_socket != -1) {
 		pthread_t hilo;
@@ -126,7 +132,7 @@ int recv_inicio_proceso(int cliente_socket,int **pid, char **nombre_archivo){
 
     *nombre_archivo = (char *)list_get(paquete, 1);
 
-	log_info(logger,"me llego el archivo: %s",*nombre_archivo);
+	log_info(loggerMemoria,"me llego el archivo: %s",*nombre_archivo);
 
 	list_destroy(paquete);
 	return 0;
@@ -140,7 +146,7 @@ int recv_fetch_instruccion(int fd_modulo, int **pid, int **pc)
 	// Obtener el path del paquete
 	*pid = (int *)list_get(paquete, 0);
 
-	log_info(logger,"me llego el pid: %d",**pid);
+	log_info(loggerMemoria,"me llego el pid: %d",**pid);
 
 	
 	*pc = (int *)list_get(paquete, 1);
@@ -157,8 +163,8 @@ int recv_fetch_instruccion(int fd_modulo, int **pid, int **pc)
     char* archivo_instruccion;
 
     bool encontrado = false; 
-    for(int i = 0; list_size(lista_procesos_activos) > i && encontrado == false ;i++){
-        t_proceso* proceso = list_get(lista_procesos_activos,i);
+    for(int i = 0; list_size(lista_ProcesosActivos) > i && encontrado == false ;i++){
+        t_proceso* proceso = list_get(lista_ProcesosActivos,i);
         if(proceso->pid == pid){
             encontrado = true;
             archivo_instruccion = proceso->archivos_instrucciones;
@@ -171,7 +177,7 @@ int recv_fetch_instruccion(int fd_modulo, int **pid, int **pc)
 }
 
 void leer_instruccion_por_pc_y_enviar(int *pid, int *pc, int fd) {
-    log_info(logger,"Recibi: %d",pid);
+    log_info(loggerMemoria,"Recibi: %d",pid);
     char *path_completa_instruccion = armar_path_instruccion(pid);
 
     FILE *archivo = fopen(path_completa_instruccion, "r");
@@ -186,7 +192,7 @@ void leer_instruccion_por_pc_y_enviar(int *pid, int *pc, int fd) {
 
     while (fgets(instruccion_leida, sizeof(instruccion_leida), archivo) != NULL) {
         if (current_pc == pc) {
-            //log_info(logger,"Instrucción %d: %s", pc, instruccion_leida);
+            //log_info(loggerMemoria,"Instrucción %d: %s", pc, instruccion_leida);
             char* instruccion = instruccion_leida;
             
 
--- memoria/src/memoria.h
+++ memoria/src/memoria.h
@@ -14,12 +14,11 @@
 
 typedef struct
 {		
-    char* puerto_escucha; 
-    int tam_pagina;
-    int tam_memoria; 
-    char* path_instrucciones;
-    int retardo_respuesta; 		
-   
+    char* PUERTO_ESCUCHA; 
+    int TAM_MEMORIA;
+    int TAM_PAGINA; 
+    char* PATH_INSTRUCCIONES;
+    int RETARDO_RESPUESTA; 		
    
 } memoria_config;
 
@@ -29,7 +28,6 @@ typedef struct
     char* archivos_instrucciones;
 
 } t_proceso;
-
 
 typedef struct
 {
@@ -42,21 +40,33 @@ typedef struct
 
 } t_instruccion;
 
-memoria_config config_valores;
-t_log* logger;
+extern memoria_config congifuracionMemoria;
+
+extern t_config* configMemoria;
+
+t_log* loggerMemoria;
+
 char* server_name;
 
-void cargar_configuracion(char* archivo_configuracion);
+void cargar_configuracion();
+
 int server_escuchar(int fd_memoria);
+
 static void procesar_conexion(void *void_args) ;
+
 int recv_fetch_instruccion(int fd_modulo, int **pid, int **pc);
+
 void leer_instruccion_por_pc_y_enviar(int *pid, int *pc, int fd) ;
+
 char *armar_path_instruccion(int pid) ;
 
-
 void obtener_instruccion(char *path_consola, int pc, int fd) ;
+
+void crearListas() ;
+
 t_instruccion armar_instruccion(char* instruccion_leida);
-t_list* lista_procesos_activos;
+
+t_list* lista_ProcesosActivos;
 
 
 
