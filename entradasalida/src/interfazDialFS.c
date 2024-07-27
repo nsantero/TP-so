87dc17b5e89fcea198acbfdcc3dd361ac6c21cf2
nsantero <nsantero@frba.utn.edu.ar>
2024-07-27 10:03:13 +0000

    agrego unas llaves y unas lineas de manejar IO

--- entradasalida/src/interfazDialFS.c
+++ entradasalida/src/interfazDialFS.c
@@ -26,6 +26,7 @@ void* manejo_interfaz_DialFS(){
         pthread_mutex_unlock(&mutex_cola_DialFS);
 
         EJECUTAR_INTERFAZ_DialFS(peticion_DialFS);
+        list_destroy_and_destroy_elements(peticion_DialFS->frames, free);
         list_destroy(peticion_DialFS->frames);
         free(peticion_DialFS->nombreArchivo);
         free(peticion_DialFS->nombre_interfaz);
